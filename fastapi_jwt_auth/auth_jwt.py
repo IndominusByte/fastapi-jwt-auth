ed83574fce634463ba557faa527af4590c19be42
IndominusByte <nyomanpradipta120@gmail.com>
2020-10-16 22:22:44 +0800

    seperate property with logic auth

--- fastapi_jwt_auth/auth_jwt.py
+++ fastapi_jwt_auth/auth_jwt.py
@@ -1,38 +1,14 @@
 import jwt
 from re import match
 from uuid import uuid4
-from pydantic import ValidationError
-from fastapi import Header, HTTPException
-from fastapi_jwt_auth.config import LoadSettings
 from datetime import datetime, timezone, timedelta
+from fastapi import Header, HTTPException
+from fastapi_jwt_auth.auth_property import AuthProperty
 from jwt.algorithms import requires_cryptography, has_crypto
+from typing import Optional, Dict, Union, Sequence
 from types import GeneratorType
-from typing import (
-    Optional,
-    Dict,
-    Union,
-    Callable,
-    List,
-    Sequence,
-)
 
-class AuthJWT:
-    _token = None
-    _secret_key = None
-    _public_key = None
-    _private_key = None
-    _algorithm = "HS256"
-    _decode_algorithms = None
-    _decode_leeway = 0
-    _encode_issuer = None
-    _decode_issuer = None
-    _decode_audience = None
-    _blacklist_enabled = None
-    _blacklist_token_checks = []
-    _token_in_blacklist_callback = None
-    _access_token_expires = timedelta(minutes=15)
-    _refresh_token_expires = timedelta(days=30)
-
+class AuthJWT(AuthProperty):
     def __init__(self,authorization: Optional[str] = Header(None)):
         """
         Get header Authorization with format 'Bearer <JWT>' and verified token, when Authorization header exists
@@ -222,46 +198,9 @@ class AuthJWT:
         except Exception as err:
             raise HTTPException(status_code=422,detail=str(err))
 
-    @classmethod
-    def load_config(cls, settings: Callable[...,List[tuple]]) -> "AuthJWT":
-        try:
-            config = LoadSettings(**{key.lower():value for key,value in settings()})
-
-            cls._secret_key = config.authjwt_secret_key
-            cls._public_key = config.authjwt_public_key
-            cls._private_key = config.authjwt_private_key
-            cls._algorithm = config.authjwt_algorithm
-            cls._decode_algorithms = config.authjwt_decode_algorithms
-            cls._decode_leeway = config.authjwt_decode_leeway
-            cls._encode_issuer = config.authjwt_encode_issuer
-            cls._decode_issuer = config.authjwt_decode_issuer
-            cls._decode_audience = config.authjwt_decode_audience
-            cls._blacklist_enabled = config.authjwt_blacklist_enabled
-            cls._blacklist_token_checks = config.authjwt_blacklist_token_checks
-            cls._access_token_expires = config.authjwt_access_token_expires
-            cls._refresh_token_expires = config.authjwt_refresh_token_expires
-        except ValidationError:
-            raise
-        except Exception:
-            raise TypeError("Config must be pydantic 'BaseSettings' or list of tuple")
-
-    @classmethod
-    def token_in_blacklist_loader(cls, callback: Callable[...,bool]) -> "AuthJWT":
-        """
-        This decorator sets the callback function that will be called when
-        a protected endpoint is accessed and will check if the JWT has been
-        been revoked. By default, this callback is not used.
-
-        *HINT*: The callback must be a function that takes *args and **kwargs argument,
-        args for object AuthJWT and this is not used, kwargs['decrypted_token'] is decode
-        JWT (python dictionary) and returns *`True`* if the token has been blacklisted,
-        or *`False`* otherwise.
-        """
-        cls._token_in_blacklist_callback = callback
-
     def blacklist_is_enabled(self) -> bool:
         """
-        Check if AUTHJWT_BLACKLIST_ENABLED in env, not None and value is true
+        Check if AUTHJWT_BLACKLIST_ENABLED not None and value is true
         """
         return self._blacklist_enabled is not None and self._blacklist_enabled == 'true'
 
---
+++ fastapi_jwt_auth/auth_property.py
@@ -0,0 +1,57 @@
+from fastapi_jwt_auth.config import LoadSettings
+from datetime import timedelta
+from pydantic import ValidationError
+from typing import Callable, List
+
+class AuthProperty:
+    _token = None
+    _secret_key = None
+    _public_key = None
+    _private_key = None
+    _algorithm = "HS256"
+    _decode_algorithms = None
+    _decode_leeway = 0
+    _encode_issuer = None
+    _decode_issuer = None
+    _decode_audience = None
+    _blacklist_enabled = None
+    _blacklist_token_checks = []
+    _token_in_blacklist_callback = None
+    _access_token_expires = timedelta(minutes=15)
+    _refresh_token_expires = timedelta(days=30)
+
+    @classmethod
+    def load_config(cls, settings: Callable[...,List[tuple]]) -> "AuthProperty":
+        try:
+            config = LoadSettings(**{key.lower():value for key,value in settings()})
+
+            cls._secret_key = config.authjwt_secret_key
+            cls._public_key = config.authjwt_public_key
+            cls._private_key = config.authjwt_private_key
+            cls._algorithm = config.authjwt_algorithm
+            cls._decode_algorithms = config.authjwt_decode_algorithms
+            cls._decode_leeway = config.authjwt_decode_leeway
+            cls._encode_issuer = config.authjwt_encode_issuer
+            cls._decode_issuer = config.authjwt_decode_issuer
+            cls._decode_audience = config.authjwt_decode_audience
+            cls._blacklist_enabled = config.authjwt_blacklist_enabled
+            cls._blacklist_token_checks = config.authjwt_blacklist_token_checks
+            cls._access_token_expires = config.authjwt_access_token_expires
+            cls._refresh_token_expires = config.authjwt_refresh_token_expires
+        except ValidationError:
+            raise
+        except Exception:
+            raise TypeError("Config must be pydantic 'BaseSettings' or list of tuple")
+
+    @classmethod
+    def token_in_blacklist_loader(cls, callback: Callable[...,bool]) -> "AuthProperty":
+        """
+        This decorator sets the callback function that will be called when
+        a protected endpoint is accessed and will check if the JWT has been
+        been revoked. By default, this callback is not used.
+        *HINT*: The callback must be a function that takes *args and **kwargs argument,
+        args for object AuthJWT and this is not used, kwargs['decrypted_token'] is decode
+        JWT (python dictionary) and returns *`True`* if the token has been blacklisted,
+        or *`False`* otherwise.
+        """
+        cls._token_in_blacklist_callback = callback
