7438bb6cada7bba0b41041e55102078a580ee54e
Yan Khachko <a@slnk.icu>
2022-02-03 22:36:28 +0000

    Trying upload your code to PyPI

--- async_fastapi_jwt_auth/__init__.py
+++ async_fastapi_jwt_auth/__init__.py
@@ -1,5 +1,5 @@
-"""FastAPI extension that provides JWT Auth support (secure, easy to use and lightweight)"""
+"""Async fork of FastAPI extension that provides JWT Auth support (secure, easy to use and lightweight)"""
 
-__version__ = "0.5.0"
+__version__ = "0.5.1"
 
 from .auth_jwt import AuthJWT
