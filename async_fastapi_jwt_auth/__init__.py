172389a25dd3e665bc9cb33919518085c7e38ad8
Yan Khachko <a@slnk.icu>
2023-01-28 00:58:16 +0200

    Version updated to 0.5.2

--- async_fastapi_jwt_auth/__init__.py
+++ async_fastapi_jwt_auth/__init__.py
@@ -1,5 +1,5 @@
 """Async fork of FastAPI extension that provides JWT Auth support (secure, easy to use and lightweight)"""
 
-__version__ = "0.5.1"
+__version__ = "0.5.2"
 
 from .auth_jwt import AuthJWT
